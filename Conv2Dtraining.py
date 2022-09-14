c0abfc9f08cf1d915066f45fdbffd99f937cdcca
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-09-14 16:01:56 +0200

    Update

--- Conv2Dtraining.py
+++ Conv2Dtraining.py
@@ -6,15 +6,12 @@ from tensorflow.keras.layers import Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, 
 import os
 import random
 import time
+from sklearn.model_selection import train_test_split
 
-def unison_shuffled_copies(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
 
 ACTIONS = ["krug", "kvadrat", "trokut"]
 
-def create_data(starting_dir="data"):
+def create_data(starting_dir="alldata"):
     training_data = {}
     for action in ACTIONS:
         if action not in training_data:
@@ -36,7 +33,7 @@ def create_data(starting_dir="data"):
     lengths = [len(training_data[action]) for action in ACTIONS]
     print(lengths)
 
-    # creating X, y 
+    # creating X, y
     combined_data = []
     for action in ACTIONS:
         for data in training_data[action]:
@@ -56,50 +53,34 @@ def create_data(starting_dir="data"):
 
 
 print("creating training data")
-traindata = create_data(starting_dir="data")
+traindata = create_data(starting_dir="alldata")
 train_X = []
 train_y = []
 for X, y in traindata:
     train_X.append(X)
     train_y.append(y)
 
-print("creating testing data")
-testdata = create_data(starting_dir="validation_data")
-test_X = []
-test_y = []
-for X, y in testdata:
-    test_X.append(X)
-    test_y.append(y)
-
-print(len(train_X))
-print(len(test_X))
 
 
 print(np.array(train_X).shape)
 print(np.array(train_y).shape)
 train_X = np.array(train_X)
-test_X = np.array(test_X)
+
 train_y = np.array(train_y)
-test_y = np.array(test_y)
 param = 90
 train_X = np.array([train_X[n:n+param] for n in range(0, len(train_X), param)])
 y = [train_y[i] for i in range(0, len(train_y), param)]
 train_y = np.array(y)
-test_X = np.array([test_X[n:n+param] for n in range(0, len(test_X), param)])
-y = [test_y[i] for i in range(0, len(test_y), param)]
-test_y = np.array(y)
 
-#unison_shuffled_copies(train_X, train_y)
+
 indices = np.random.permutation(len(train_X))
 train_X = train_X[indices]
 train_y = train_y[indices]
 
-print("shape:", train_X.shape)
-print("shape:", train_y.shape)
-print("shapet:", test_X.shape)
-print("shapet:", test_y.shape)
+X_train, X_test, y_train, y_test = train_test_split(train_X, train_y, train_size=0.70)
+print("shape:", X_train.shape)
+print("shape:", y_train.shape)
 
-# samples, rows, cols, channels
 opt = tf.keras.optimizers.Adam(
     learning_rate=0.0007,
     beta_1=0.9,
@@ -125,7 +106,6 @@ model.add(MaxPooling2D((5, 5), padding="same"))
 
 model.add(Flatten())
 
-#model.add(Dense(256))
 model.add(Dense(128))
 model.add(Dense(64))
 model.add(Dense(3, activation="softmax"))
@@ -135,15 +115,15 @@ model.compile(loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 epochs = 10
+mean = 0
 batch_size = 32
 model.summary()
 for epoch in range(epochs):
-    model.fit(train_X, train_y, batch_size=batch_size, epochs=1, validation_data=(test_X, test_y))
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=1, validation_split=0.2)
     print((epoch+1)*10, "%")
-    score = model.evaluate(test_X, test_y, batch_size=batch_size)
-
+    score = model.evaluate(X_test, y_test, batch_size=batch_size)
+    mean += score[1]
     MODEL_NAME = f"new_models/{round(score[1]*100,2)}-acc-{epochs}epoch-{int(time.time())}-loss-{round(score[0],2)}.model"
-    model.save(MODEL_NAME)
+   # model.save(MODEL_NAME)
 
-
-
+print(round(mean/10*100, 2),"%")
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Klasifikacija geometrijskih likova koristeći EEG podatke
 
 Ovaj projekt istražuje ideju o klasifikaciji geometrijskih tijela pomoću EEG podataka pri čemu se klasificiraju krug, trokut i kvadrat.
-EEG podaci prikazani su u protoku voltaže. Snimanje EEG podataka napravljeno je putem uređaja OpenBCI Cyton + Daisy pri čemu su podaci snimani sa jednim ispitanikom.
+EEG podaci prikazani su u protoku voltaže. Snimanje EEG podataka napravljeno je putem uređaja OpenBCI Cyton + Daisy pri čemu su podaci snimani sa jednim ispitanikom. Postavljeno je dodatno pitanje, pitanje relevantnosti EEG podataka kroz vrijeme pri čemu se snimanje podataka sastoji od 6 sesija. Prve tri sesije snimljene su 3.4.2022. i 10.5.2022., a ostale su snimljene 9.9.2022.
 
 ## Model
 
@@ -16,9 +16,7 @@ Prikaz arhitekture CNN modela
 ## Podaci
 
 
-- Skup podataka(dataset) za treniranje lociran je u [data/](data/) direktoriju.
-- Skup podataka za validaciju lociran je u [validation_data/](validation_data/) direktoriju.
-- Skup podataka za test lociran je u [test_dataset/](test_dataset/)
+- Skup podataka(dataset) korišten u projektu nalazi se u [alldata/](alldata/) direktoriju.
 
 ## Organizacija
 Sveučilište Jurja Dobrile u Puli </br>
